8d53e08672ee5569847247aa83a75afa044f172a
mario-george <vortexspades@gmail.com>
2023-06-22 00:50:59 +0300

    addedPlacesRoutes

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const placesRoutes = require("./routes/places-routes");
 const app = express();
 // middleware for all incoming requests
 // app.use
-
 // middleware for specific http requests
-// app.get app.post app.put app.delete app.patch 
-
+// app.get app.post app.put app.delete app.patch
 
 // body-parser will parse the request body
+
+app.use('/api/places',placesRoutes);
+app.listen(3003);
--- routes/places-routes.js
+++ routes/places-routes.js
@@ -2,9 +2,40 @@ const express = require("express");
 const router = express.Router();
 // it takes a filter or route if it reaches the route it will execute the function in the second parameter
 // REST API exchanges data with json format
+const DummyPlaces = [
+  { id: "p1", title: "empire state building", creator: "u1" },
+  { id: "p2", title: "grand prismatic building", creator: "u2" },
+];
+/* router.get("/", (req, res, next) => {
+  console.log("GET REQUEST");
+  res.json({ message: "GET REQUEST" });
+  // default response status is 200
+}); */
+// ctrl shift a multi line comment
 
-
-router.get("/", (req, res, next) => {
-    console.log("GET REQUEST")
-    res.json({message:"GET REQUEST"})
+router.get("/:placeID", (req, res, next) => {
+  const placeID = req.params.placeID;
+  const place = DummyPlaces.find((place) => {
+    return place.id === placeID;
+  });
+  if (!place) {
+    return res.json({ message: "Place not found" });
+  } else {
+    return res.json({ place });
+  }
 });
+router.get("/user/:userID", (req, res, next) => {
+  const { userID } = req.params;
+  const place = DummyPlaces.find((place) => {
+    return place.creator === userID;
+  });
+  if (!place) {
+    return res.json({ message: "Place not found by this user" });
+  } else {
+    return res.json({ place });
+  }
+});
+router.get("/", (req, res, next) => {
+  return res.json({ places: DummyPlaces });
+});
+module.exports = router;
