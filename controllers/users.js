e4f70cf4da9a2cbe53ef337031904a1542a57cfd
mario-george <vortexspades@gmail.com>
2024-01-01 12:38:40 +0200

    create a user (sign up ) controller function

--- controllers/users.js
+++ controllers/users.js
@@ -27,20 +27,46 @@ const getAllUsers = async (req, res, next) => {
     allUsers: allUsers.map((user) => user.toObject({ getters: true })),
   });
 };
-const signup = (req, res, next) => {
-  const { email, password, name } = req.body;
+const signup = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return next(new HttpError("Invalid input please check your data", 422));
   }
-  const createdUser = {
-    id: uuid.v4(),
+  const { email, password, name, places } = req.body;
+
+  let existingUser;
+  try {
+    existingUser = await User.findOne({ email: email });
+  } catch (err) {
+    const error = new HttpError(
+      "Signing up failed, please try again later.",
+      500
+    );
+    return next(error);
+  }
+  if (existingUser) {
+    const error = new HttpError(
+      "User exists already, please login instead.",
+      422
+    );
+    return next(error);
+  }
+  const createdUser = new User({
     email: email,
     password: password,
     name: name,
-  };
-  DummyUsers.push(createdUser);
-  res.status(201).json({ user: createdUser });
+    places,
+    image:
+      "https://www.google.com/url?sa=i&url=https%3A%2F%2Fin.cdgdbentre.edu.vn%2Frandom-anime-character-qbx135oe%2F&psig=AOvVaw1ciu2tyNyfhJBew5LB-gTZ&ust=1704191831767000&source=images&cd=vfe&ved=0CBIQjRxqFwoTCLjbj7n_u4MDFQAAAAAdAAAAABAJ",
+  });
+
+  try {
+    await createdUser.save();
+  } catch (err) {
+    const error = new HttpError("Signing up failed, please try again.", 500);
+    return next(error);
+  }
+  res.status(201).json({ user: createdUser.toObject({ getters: true }) });
 };
 const login = (req, res, next) => {
   const { email, password } = req.body;
