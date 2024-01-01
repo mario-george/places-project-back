0edd089ba83d2980174c0a87678cda8705d1c365
mario-george <vortexspades@gmail.com>
2024-01-01 11:22:13 +0200

    update plave by id controller function

--- controllers/places.js
+++ controllers/places.js
@@ -85,26 +85,38 @@ const createPlace = async (req, res, next) => {
 
   res.status(201).json({ place: createdPlace });
 };
-const updatePlace = (req, res, next) => {
+const updatePlace = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return next(new HttpError("Invalid input please check your data", 422));
   }
   const { title, description } = req.body;
-  const updatedPlace = { ...DummyPlaces.find((p) => p.id === req.params.pid) };
-  //   the spread operator takes a copy of all key value pairs of the object
 
-  const updatedPlaceIndex = DummyPlaces.findIndex(
-    (p) => p.id === req.params.pid
-  );
-  updatedPlace.title = title;
-  updatedPlace.description = description;
-  //  we replaced the whole element of the array with the updated place
-  DummyPlaces[updatedPlaceIndex] = updatedPlace;
-  if (updatedPlaceIndex === -1) {
-    return next(new HttpError("Could not find place with that id", 404));
+  let place;
+  try {
+    place = await Place.findById(req.params.pid);
+  } catch (err) {
+    const error = new HttpError(
+      "Something went wrong, could not update place.",
+      500
+    );
+    return next(error);
   }
-  return res.status(200).json({ message: "success", place: updatedPlace });
+
+  place.title = title;
+  place.description = description;
+
+  try {
+    await place.save();
+  } catch (err) {
+    const error = new HttpError(
+      "Something went wrong, could not update place.",
+      500
+    );
+    return next(error);
+  }
+
+  res.json({ place: place.toObject({ getters: true }) });
 };
 const deletePlace = (req, res, next) => {
   const deletedPlace = DummyPlaces.find((p) => p.id === req.params.pid);
