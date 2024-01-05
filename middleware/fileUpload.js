8501886c9df868d632c1a65b17a7980fba69611c
mario-george <vortexspades@gmail.com>
2024-01-05 21:01:45 +0200

    specifing filename randomly and the appropriate extention

--- middleware/fileUpload.js
+++ middleware/fileUpload.js
@@ -1,7 +1,11 @@
 const multer = require("multer");
 const uuid = require("uuid");
 
-
+const mimeTypeMapperToExt = {
+  "image/png": "png",
+  "image/jpeg": "jpeg",
+  "image/jpg": "jpg",
+};
 
 // multer (group of middlewares) takes a config object to set it
 // dest: 'uploads/images' is a built in multer function that will create a folder called uploads and inside it a folder called images and will store the images there
@@ -10,7 +14,11 @@ const fileUpload = multer({
     // specify destunation of the images
     callback(null, "uploads/images");
   },
-
+  filename: (req, file, cb) => {
+    // specify the name of the images to be random
+    const ext = mimeTypeMapperToExt[file.mimetype];
+    cb(null, uuid.v4() + "." + ext);
+  },
 });
 /* 
 limits:500000 is a built in multer function that will limit the size of the file to 500000 bytes
