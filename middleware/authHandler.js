83c11d3102a5c4ae7685b0864577c2eae4d65b0d
mario-george <vortexspades@gmail.com>
2024-01-07 00:44:50 +0200

    use jwt.verify to verify the token using the jwt secret key used to create the token

--- middleware/authHandler.js
+++ middleware/authHandler.js
@@ -11,7 +11,14 @@ const authHandler = (req, res, next) => {
     // headers properties are case insensitive
 
     const token = req.headers.authorization.split(" ")[1]; 
-
+    if (!token) {
+      throw new Error("Authentication failed!");
+    }
+    const tokenDecoded = jwt.verify(token, process.env.JWT_KEY);
+    
+    // attach a property to the request object that contains the user's ID 
+    req.userData = { userId: tokenDecoded.userId };
+    next();
   } catch (err) {
     const error = new HttpError("Authentication failed!", 403);
     return next(error);
