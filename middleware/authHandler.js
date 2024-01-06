1d3990f11e21bd478980c21e63647dd7e0092577
mario-george <vortexspades@gmail.com>
2024-01-07 00:43:42 +0200

    created a method and extracted the  token from the headers of the received http request

--- middleware/authHandler.js
+++ middleware/authHandler.js
@@ -1,5 +1,21 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
-const HttpError = require('../models/http-error');
+const HttpError = require("../models/http-error");
 
-module.exports ={}
+const authHandler = (req, res, next) => {
+  if (req.method === "OPTIONS") {
+    return next();
+  }
+  try {
+    // token must be sent from the front-end by attaching in the headers object { Authorization: 'Bearer TOKEN'}
+    // headers properties are case insensitive
+
+    const token = req.headers.authorization.split(" ")[1]; 
+
+  } catch (err) {
+    const error = new HttpError("Authentication failed!", 403);
+    return next(error);
+  }
+};
+
+module.exports = authHandler;
