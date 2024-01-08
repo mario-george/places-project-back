fef0358b501d8805cdeb484dcce0e89629b869c9
mario-george <vortexspades@gmail.com>
2024-01-08 13:22:30 +0200

    created an async function by getting the address and passing it to the geocoder.geocode method

--- middleware/latLngExtraction.js
+++ middleware/latLngExtraction.js
@@ -6,4 +6,13 @@ const options = {
 
 const geocoder = NodeGeocoder(options);
 
+async function getGeocodingData(address) {
+  try {
+    const res = await geocoder.geocode(address);
+    return res[0];
+  } catch (error) {
+    console.log(error);
+  }
+}
 
+module.exports = getGeocodingData;
